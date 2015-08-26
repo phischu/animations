c88d6fe16cbee40c50abc9eed628c671147ddc9f
Philipp Schuster <phischu3000@googlemail.com>
2015-08-26 22:28:33 +0200

    experiments in io

--- Main.hs
+++ Main.hs
@@ -4,8 +4,11 @@ module Main where
 import Control.Monad.Free
 import Control.Comonad.Cofree
 import Control.Monad
+import Control.Monad.Trans.Class
 import Control.Monad.Trans.Maybe
 import Control.Applicative
+import Data.IORef
+import Control.Concurrent
 
 
 type Event = Free (MaybeT IO)
@@ -46,35 +49,64 @@ whenJust (Nothing :< b) = Free e :< b' where
 
 runBehavior :: Behavior (Event a) -> IO a
 runBehavior (Pure a :< _) = return a
-runBehavior (_ :< MaybeT b') = do
-    mb <- b'
-    case mb of
-        Nothing -> error "loop 2"
-        Just b -> runBehavior b
+runBehavior (_ :< b) =
+    runMaybeT b >>= maybe (error "runBehavior loop") runBehavior
 
 
-test :: Int -> Behavior (Event ())
-test n = whenTrue (do
-    i <- count
-    return (i == n))
+runEvent :: Event a -> IO a
+runEvent (Pure a) = return a
+runEvent (Free (MaybeT io)) = io >>= maybe (error "runEvent loop") runEvent
 
-count :: Behavior Int
-count = loop 0
+async :: IO a -> IO (Event a)
+async io = do
+    resultRef <- newIORef Nothing
+    forkIO (io >>= writeIORef resultRef . Just)
+    b <- poll (readIORef resultRef)
+    return (now (whenJust b))
 
-loop :: Int -> Behavior Int
-loop i = always i `switch` later (occured (loop (i+1)))
+poll :: IO a -> IO (Behavior a)
+poll io = do
+    a <- io
+    return (a :< lift (poll io))
+
+plan :: Event (IO a) -> IO (Event a)
+plan (Pure io) = fmap occured io
+plan (Free e) = runMaybeT e >>= maybe (return never) plan
+
+
+n :: Int
+n = 11
+
+main :: IO ()
+main = do
+    e <- test n
+    runEvent e
+
+{-
+test :: Int -> Now (Event ())
+test n = do b <- count 
+            e <- sample (when ((n ==) <$> b))
+            return e
+
+count :: Now (Behavior Int)
+count = loop 0 where
+  loop i =  do  e <- async (return ())
+                e'<- planNow (loop (i+1) <$ e)
+                return (pure i `switch` e')
+-}
+
+test :: Int -> IO (Event ())
+test n = do b <- count
+            e <- return (now (whenTrue ((n ==) <$> b)))
+            return e
+
+count :: IO (Behavior Int)
+count = loop 0 where
+    loop i = do e <- async (return ())
+                e' <- plan (loop (i+1) <$ e)
+                return (pure i `switch` e')
 
 whenTrue :: Behavior Bool -> Behavior (Event ())
 whenTrue = whenJust . fmap boolToMaybe where
     boolToMaybe True = Just ()
     boolToMaybe False = Nothing
-
-p :: Behavior (Int,Int)
-p = do
-    x <- always 5
-    y <- count
-    return (x,y)
-
-
-main :: IO ()
-main = putStrLn "hi"

980f436c148644b01426b65e07d59ac804033051
Philipp Schuster <phischu3000@googlemail.com>
2015-09-06 17:35:31 -0700

    once

--- Main.hs
+++ Main.hs
@@ -5,14 +5,52 @@ import Control.Comonad
 import Control.Applicative
 import Data.IORef
 import Control.Concurrent
+import System.IO.Unsafe
 
-
+-- http://blog.openendings.net/2014/10/two-sound-axiom-systems-for-linear.html
+-- http://plato.stanford.edu/entries/logic-temporal/
+-- Completeness?
+-- https://en.wikipedia.org/wiki/Normal_modal_logic
 
 type Behavior = BehaviorT Next
 
 type Event = EventT Next
 
-type Next = IO
+type Next = Once
+
+data Once a = Once { runOnce :: IO a }
+
+instance Functor Once where
+    fmap f (Once io) = Once (fmap f io)
+
+instance Applicative Once where
+    pure a = Once (pure a)
+    Once f <*> Once x = Once (f <*> x)
+
+instance Monad Once where
+    return = pure
+    ox >>= ok = joinOnce (fmap ok ox)
+
+joinOnce :: Once (Once a) -> Once a
+joinOnce (Once ioonce) = Once (do
+    Once io <- ioonce
+    a <- io
+    return a)
+
+liftOnce :: IO a -> Once a
+liftOnce io = unsafePerformIO (do 
+    r <- newIORef Nothing
+    return (Once (do
+        ma <- readIORef r
+        case ma of
+            Nothing -> do
+                a <- io
+                writeIORef r (Just a)
+                return a
+            Just a -> return a)))
+
+liftOften :: IO a -> Once a
+liftOften = Once
 
 now :: Behavior a -> a
 now = _now
@@ -21,14 +59,14 @@ future :: Behavior a -> Next (Behavior a)
 future = _future
 
 always :: a -> Behavior a
-always a = a `andThen` return (always a)
+always a = a `andThen` pure (always a)
 
 andThen :: a -> Next (Behavior a) -> Behavior a
 andThen = AndThen
 
 
 never :: Event a
-never = Later (return never)
+never = Later (pure never)
 
 occured :: a -> Event a
 occured = Occured
@@ -38,7 +76,7 @@ later = Later
 
 
 delay :: a -> Next a
-delay = return
+delay = pure
 
 
 data EventT m a =
@@ -79,36 +117,38 @@ whenJust b = case now b of
         b' = fmap whenJust (future b)
         e = fmap now b'
 
-plan :: Event (IO a) -> IO (Event a)
+plan :: Event (Next a) -> Next (Event a)
 plan e = case e of
     Occured io -> fmap occured io
-    Later nextEvent -> return (later (nextEvent >>= plan))
+    Later nextEvent -> pure (later (nextEvent >>= plan))
 
-poll :: Behavior (IO a) -> IO (Behavior a)
+poll :: Behavior (Next a) -> Next (Behavior a)
 poll b = do
     a <- now b
-    return (a `andThen` (future b >>= poll))
+    pure (a `andThen` (future b >>= poll))
 
-async :: IO a -> IO (Event a)
+async :: IO a -> Next (Event a)
 async io = do
-    resultRef <- newIORef Nothing
-    forkIO (io >>= writeIORef resultRef . Just)
-    b <- poll (always (readIORef resultRef))
+    r <- liftOnce (do
+        resultRef <- newIORef Nothing
+        forkIO (io >>= writeIORef resultRef . Just)
+        return resultRef)
+    b <- poll (always (liftOften (readIORef r)))
     return (later (pure (now (whenJust b))))
 
 
 waitEvent :: Event a -> IO ()
 waitEvent (Occured _) = return ()
-waitEvent (Later me) = me >>= waitEvent
+waitEvent (Later me) = runOnce me >>= waitEvent
 
 runBehavior :: (Show a) => Behavior a -> IO ()
 runBehavior(a `AndThen` mb) = do
     print a
-    mb >>= runBehavior
+    runOnce mb >>= runBehavior
 
 runEvent :: (Show a) => Event a -> IO ()
 runEvent (Occured a) = print a
-runEvent (Later me) = me >>= (\e -> do
+runEvent (Later me) = runOnce me >>= (\e -> do
     putStrLn "."
     threadDelay 500000
     runEvent e)
@@ -117,8 +157,7 @@ runEvent (Later me) = me >>= (\e -> do
 
 main :: IO ()
 main = do
-    e <- test 11000
-    waitEvent e
+    waitEvent (later (test 11000))
 
 {-
 test :: Int -> Now (Event ())
@@ -133,12 +172,12 @@ count = loop 0 where
                 return (pure i `switch` e')
 -}
 
-test :: Int -> IO (Event ())
+test :: Int -> Next (Event ())
 test n = do b <- count
             e <- return (now (whenTrue ((n ==) <$> b)))
             return e
 
-count :: IO (Behavior Int)
+count :: Next (Behavior Int)
 count = loop 0 where
     loop i = do e <- async (return ())
                 e' <- plan (loop (i+1) <$ e)
@@ -149,11 +188,12 @@ whenTrue = whenJust . fmap boolToMaybe where
     boolToMaybe True = Just ()
     boolToMaybe False = Nothing
 
-
+{-
 register :: Event a -> ((a -> IO ()) -> IO ())
 register event handler = do
     plan (fmap handler event)
     return ()
+-}
 
 callback :: IO (a -> IO (), Event a)
 callback = error "not implemented: callback"
@@ -178,9 +218,18 @@ wait e = case e of
     Occured a -> always (occured a)
     Later e' -> later e' `andThen` fmap wait e'
 
+snapshot :: Behavior a -> Event () -> Event a
+snapshot b e = case e of
+    Occured () -> Occured (now b)
+    Later nextEvent -> Later (liftA2 snapshot (future b) nextEvent)
+
 {-
 what :: IO ()
 what = do
     inputs <- poll (always getLine)
     runBehavior (accum "" (fmap (++) inputs))
 -}
+
+indo :: Behavior (a -> Next a) -> a -> Behavior a
+indo = undefined
+

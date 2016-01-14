665eaf929adf9c94a2d7ee43689be81ce9c3ec14
Philipp Schuster <phischu3000@googlemail.com>
2016-01-14 14:06:04 +0100

    Parametrize Behaiors and Events over Next.

--- Main.hs
+++ Main.hs
@@ -11,134 +11,99 @@ import Data.Time
 import Control.Monad
 
 
-type Behavior = BehaviorT Next
-
-type Event = EventT Next
-
-newtype Next a = Next {runNext :: IO a}
-  deriving (Functor, Applicative)
-
-now :: Behavior a -> a
-now = _now
-
-future :: Behavior a -> Next (Behavior a)
-future = _future
-
-always :: a -> Behavior a
-always a = a `andThen` pure (always a)
-
-andThen :: a -> Next (Behavior a) -> Behavior a
-andThen = AndThen
-
-
-never :: Event a
-never = Later (pure never)
-
-occured :: a -> Event a
-occured = Occured
-
-later :: Next (Event a) -> Event a
-later = Later
-
-
-delay :: a -> Next a
-delay = pure
-
-syncIO :: IO a -> Next a
-syncIO = Next
-
-data EventT m a =
+data Event next a =
     Occured a |
-    Later (m (EventT m a))
+    Later (next (Event next a))
       deriving (Functor)
 
-instance (Applicative m) => Applicative (EventT m) where
+instance (Applicative next) => Applicative (Event next) where
     pure = Occured
     Occured f <*> Occured x = pure (f x)
     Occured f <*> Later mx = Later (liftA2 (<*>) (pure (Occured f)) mx)
     Later mf <*> Occured x = Later (liftA2 (<*>) mf (pure (Occured x)))
     Later mf <*> Later mx = Later (liftA2 (<*>) mf mx)
 
-instance (Applicative m) => Monad (EventT m) where
+instance (Applicative next) => Monad (Event next) where
     return = pure
     Occured a >>= mf = mf a
     Later ma >>= mf = Later (fmap (>>= mf) ma)
 
-data BehaviorT m a = AndThen {
-    _now :: a,
-    _future :: m (BehaviorT m a)}
+data Behavior next a = AndThen {
+    now :: a,
+    future :: next (Behavior next a)}
       deriving (Functor)
 
-instance (Applicative m) => Applicative (BehaviorT m) where
+instance (Applicative next) => Applicative (Behavior next) where
     pure a = a `AndThen` pure (pure a)
     (f `AndThen` mf) <*> (x `AndThen` mx) = f x `AndThen` liftA2 (<*>) mf mx
 
 
--- type Pull = Reader Time
--- Behavior Pull a
--- Behavior Push a
--- pullIO :: IO a -> Pull a
--- synchronous
 
--- type Push = Writer Time
--- instance Monoid Time where
---   mempty = negative infinity
---   mappend = max
--- Event Pull a
--- Event Push a
--- pushIO :: IO a -> Push a
--- asynchrounous
+always :: (Applicative next) => a -> Behavior next a
+always a = a `AndThen` pure (always a)
 
 
-switch :: Behavior a -> Event (Behavior a) -> Behavior a
-switch b e = case e of
-    Occured b' -> b'
-    Later e' -> now b `AndThen` liftA2 switch (future b) e'
-
-whenJust :: Behavior (Maybe a) -> Event a
-whenJust behavior = case now behavior of
-    Just a -> occured a
-    Nothing -> later (fmap whenJust (future behavior))
-
-plan :: Event (Next a) -> Next (Event a)
-plan event = case event of
-  Occured nextA -> fmap occured nextA
-  Later nextEvent -> fmap (later . plan) nextEvent
-
-poll :: Behavior (Next a) -> Next (Behavior a)
-poll behavior =
-  liftA2 andThen (now behavior) (fmap poll (future behavior))
-
-runEvent :: Event a -> IO a
-runEvent event = case event of
-  Occured a -> return a
-  Later nextEvent -> do
-    event' <- runNext nextEvent
-    runEvent event'
-
-nextLine :: Next (Event String)
-nextLine = plan (Occured (syncIO getLine))
-
-currentTime :: Next (Behavior UTCTime)
-currentTime = poll (always (syncIO getCurrentTime))
-
-nextPutStrLn :: String -> Next (Event ())
-nextPutStrLn message = plan (Occured (syncIO (putStrLn message)))
+never :: (Applicative next) => Event next a
+never = Later (pure never)
 
 
-whenTrue :: Behavior Bool -> Event ()
-whenTrue behavior = case now behavior of
-  True -> occured ()
-  False -> later (fmap whenTrue (future behavior))
+syncIO :: IO a -> IO a
+syncIO = id
 
-sample :: Event a -> Behavior b -> Event (a, b)
+type Time = Float
+
+type Push a = (Time, a)
+
+type Pull a = Time -> a
+
+
+switch :: (Applicative next) => Behavior next a -> Event next (Behavior next a) -> Behavior next a
+switch _ (Occured behavior') =
+  behavior'
+switch (a `AndThen` nextBehavior) (Later nextEvent) =
+  a `AndThen` liftA2 switch nextBehavior nextEvent
+
+whenJust :: (Functor next) => Behavior next (Maybe a) -> Event next a
+whenJust (Just a `AndThen` nextBehavior) =
+  Occured a
+whenJust (Nothing `AndThen` nextBehavior) =
+  Later (fmap whenJust nextBehavior)
+
+whenTrue :: (Functor next) => Behavior next Bool -> Event next ()
+whenTrue (True `AndThen` _) =
+  Occured ()
+whenTrue (False `AndThen` nextBehavior) =
+  Later (fmap whenTrue nextBehavior)
+
+sample :: (Applicative next) => Event next a -> Behavior next b -> Event next (a, b)
 sample (Occured a) (AndThen b _) =
   Occured (a, b)
 sample (Later nextEvent) (AndThen _ nextBehavior) =
   Later (liftA2 sample nextEvent nextBehavior)
 
+plan :: (Functor next) => Event next (next a) -> next (Event next a)
+plan (Occured nextA) =
+  fmap Occured nextA
+plan (Later nextEvent) =
+  fmap (Later . plan) nextEvent
 
-loop :: Event String -> Behavior UTCTime -> Event ()
+poll :: (Applicative next) => Behavior next (next a) -> next (Behavior next a)
+poll (nextA `AndThen` nextBehavior) =
+  liftA2 AndThen nextA (fmap poll nextBehavior)
+
+runEvent :: Event IO a -> IO a
+runEvent (Occured a) =
+  return a
+runEvent (Later nextEvent) =
+  nextEvent >>= runEvent
+
+nextLine :: IO (Event IO String)
+nextLine = plan (Occured (syncIO getLine))
+
+currentTime :: IO (Behavior IO UTCTime)
+currentTime = poll (always (syncIO getCurrentTime))
+
+loop :: Event IO String -> Behavior IO UTCTime -> Event IO ()
 loop (Occured "exit") _ = do
   Occured ()
 loop (Occured message) (AndThen time futureTime) =
@@ -156,46 +121,3 @@ main = runEvent (Later (
 
 
 
-{-
-test :: Int -> Now (Event ())
-test n = do b <- count 
-            e <- sample (when ((n ==) <$> b))
-            return e
-
-count :: Now (Behavior Int)
-count = loop 0 where
-  loop i =  do  e <- async (return ())
-                e'<- planNow (loop (i+1) <$ e)
-                return (pure i `switch` e')
--}
-
-
-callback :: IO (a -> IO (), Event a)
-callback = error "not implemented: callback"
-
-whenCalled :: ((a -> IO ()) -> IO ()) -> IO (Event a)
-whenCalled k = do
-    (handler, event) <- callback
-    k handler
-    return event
-
-
-accum :: a -> Behavior (a -> a) -> Behavior a
-accum a b =
-    let f = now b
-        b' = future b
-        a' = f a
-    in a' `andThen` fmap (accum a') b'
-
-
-wait :: Event a -> Behavior (Event a)
-wait e = case e of
-    Occured a -> always (occured a)
-    Later e' -> later e' `andThen` fmap wait e'
-
-{-
-what :: IO ()
-what = do
-    inputs <- poll (always getLine)
-    runBehavior (accum "" (fmap (++) inputs))
--}
